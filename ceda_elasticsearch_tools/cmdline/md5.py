c1e024012bd4e6ff43e73e228cbce6c265d53cd9
rsmith013 <richard.d.smith@stfc.ac.uk>
2018-01-18 15:11:42 +0000

    Added code to calculate md5s

--- ceda_elasticsearch_tools/cmdline/md5.py
+++ ceda_elasticsearch_tools/cmdline/md5.py
@@ -7,22 +7,25 @@ Usage:
     update_md5.py -h | --help
     update_md5.py --version
     update_md5.py
-                   (-s SPOT         | --spot   SPOT        )
-                   (-a ARCHIVE_ROOT         | --archive_root   ARCHIVE_ROOT        )
-                   (-i INDEX        | --index   INDEX      )
-                   (-o OUTPUT                              )
-                   [-h HOSTNAME     | --hostname HOSTNAME  ]
-                   [-p PORT         | --port    PORT       ]
+                   (-i INDEX                | --index   INDEX                   )
+                   (-o OUTPUT               | --output OUTPUT                   )
+                   [-s SPOT                 | --spot   SPOT                     ]
+                   [-a ARCHIVE_ROOT         | --archive_root   ARCHIVE_ROOT     ]
+                   [-h HOSTNAME             | --hostname HOSTNAME               ]
+                   [-p PORT                 | --port    PORT                    ]
+                   [--pagefile PAGE_FILE                                        ]
 
 
 Options:
     --help              Display help
     --version           Show Version
-    -s  --spot          Spot name
     -i  --index         Elasticsearch index to test
-    -o                  Logging output directory.
+    -o  --output        Logging output directory.
+    -s  --spot          Spot name
+    -a  --archive_root  Root path on the archive mapped to spot.
     -h  --hostname      Elasticsearch host to query [default: jasmin-es1.ceda.ac.uk]
     -p  --port          Elasticsearch read/write port [default: 9200]
+    --pagefile          File containing id and path information
 
 """
 from docopt import docopt
@@ -30,6 +33,8 @@ from ceda_elasticsearch_tools.core import updater
 from datetime import datetime
 import os, logging
 from ceda_elasticsearch_tools.cmdline import __version__
+import hashlib
+import simplejson as json
 
 def logger_setup(log_dir):
 
@@ -47,6 +52,13 @@ def logger_setup(log_dir):
 
     return logging.getLogger(__name__)
 
+def file_md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname,'rb') as f:
+        for chunk in iter(lambda: f.read(4096),b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
 def main():
     arguments = docopt(__doc__, version=__version__)
 
@@ -55,6 +67,7 @@ def main():
     log_dir = arguments["OUTPUT"]
     spot = arguments["SPOT"]
     archive_root = arguments["ARCHIVE_ROOT"]
+    pagefile = arguments["PAGE_FILE"]
 
     # Set defaults
     if arguments["HOSTNAME"] == None:
@@ -73,14 +86,54 @@ def main():
     # Initialise the elasticsearch updater instance
     update = updater.ElasticsearchUpdater(index=index, host=host, port=port)
 
-    logger.info('Analysing {}'.format(spot))
 
-    # Update MD5s for given spot
-    start = datetime.now()
-    update.update_md5(spot, archive_root)
+    if pagefile == None:
+        # Are processing log files by spot
+
+        logger.info('Analysing {}'.format(spot))
+
+        # Update MD5s for given spot
+        start = datetime.now()
+        update.update_md5(spot, archive_root)
 
 
-    logger.info("Spot: {}  Update took: {}".format(spot, datetime.now() - start))
+        logger.info("Spot: {}  Update took: {}".format(spot, datetime.now() - start))
+
+    else:
+        # Are calculating MD5s from scratch.
+        with open(os.path.join('page_files',pagefile)) as reader:
+            lines = reader.readlines()
+
+            # Check md5s
+            update_total = 0
+            md5_json = ""
+
+            try:
+                for file in lines:
+                    line_split = file.split(',')
+
+                    es_id = line_split[0].strip()
+                    file_path = line_split[1].strip()
+
+                    md5 = file_md5(file_path)
+
+                    if md5:
+                        update_total += 1
+
+                        index = json.dumps({"update": {"_id": es_id, "_type": "file"}}) + "\n"
+                        md5_field = json.dumps({"source": {"doc": {"info": {"md5": md5}}}}) + "\n"
+                        md5_json += index + md5_field
+
+                    if update_total > 800:
+                        update.make_bulk_update(md5_json)
+                        md5_json = ""
+                        update_total = 0
+
+                if md5_json:
+                    update.make_bulk_update(md5_json)
+
+            except Exception, msg:
+                logger.error(msg)
 
 
 if __name__ == "__main__":
--- ceda_elasticsearch_tools/cmdline/update_md5.py
+++ ceda_elasticsearch_tools/cmdline/update_md5.py
@@ -1,15 +1,17 @@
 """
 Update MD5. Use this script to scan a spot logfile, gather the md5 checksums, query Elasticsearch to see if md5 matches
 and if not, update it using the Elasticsearch Bulk API.
+Use the -c | --calculate flag in order to bypass the log files and just calculate the md5s.
 
 Usage:
     update_md5.py -h | --help
     update_md5.py --version
     update_md5.py
-                   (-i INDEX        | --index   INDEX )
-                   (-o OUTPUT                           )
-                   [-h HOSTNAME     | --hostname HOSTNAME ]
-                   [-p PORT         | --port    PORT ]
+                   (-i INDEX        | --index   INDEX       )
+                   (-o OUTPUT                               )
+                   [-h HOSTNAME     | --hostname HOSTNAME   ]
+                   [-p PORT         | --port    PORT        ]
+                   [-c              | --calculate           ]
 
 
 Options:
@@ -19,16 +21,19 @@ Options:
     -o                  Logging output directory.
     -h  --hostname      Elasticsearch host to query [default: jasmin-es1.ceda.ac.uk]
     -p  --port          Elasticsearch read/write port [default: 9200]
+    -c  --calculate     Calculate the MD5s from scratch and ignore the log files
 
 """
 from docopt import docopt
 from ceda_elasticsearch_tools.core import log_reader
-from ceda_elasticsearch_tools.core import updater
 from datetime import datetime
 import os, logging
 from ceda_elasticsearch_tools.cmdline import __version__
-from tqdm import tqdm
 import subprocess
+import simplejson as json
+from ceda_elasticsearch_tools.core.updater import ElasticsearchUpdater
+import math
+from ceda_elasticsearch_tools.core.util import ProgressBar
 
 
 def logger_setup(log_dir):
@@ -47,29 +52,10 @@ def logger_setup(log_dir):
     return logging.getLogger(__name__)
 
 
-def main():
-    arguments = docopt(__doc__, version=__version__)
+def update_from_logs(arguments, index, log_dir):
 
-    # Extract commandline args.
-    index = arguments["INDEX"]
-    log_dir = arguments["OUTPUT"]
-
-    if arguments["HOSTNAME"] == None:
-        host = "jasmin-es1.ceda.ac.uk"
-    else:
-        host = arguments["HOSTNAME"]
-
-    if arguments["PORT"] == None:
-        port = 9200
-    else:
-        port = arguments["PORT"]
-
-    # setup logging
-    logger = logger_setup(log_dir)
-
+    # Download list of current spots
     spots = log_reader.SpotMapping()
-
-    logger.info("Updating {} index with md5 checksums.".format(index))
 
     # Load completed spots
     try:
@@ -81,22 +67,141 @@ def main():
 
     with open('completed_spots.txt', 'a') as comp_spot_output:
 
-        begin = datetime.now()
         for spot in spots:
             # Only run non-complete spots
             if spot not in completed_spots:
                 subprocess.call('md5.py -s {spot} -a {archive_root} -i {index} -o {log_dir}'.format(
-                        spot=spot,
-                        archive_root=spots.get_archive_root(
-                            spot),
-                        index=index,
-                        log_dir=log_dir),
+                    spot=spot,
+                    archive_root=spots.get_archive_root(spot),
+                    index=index,
+                    log_dir=log_dir),
                     shell=True)
 
                 # Add completed spot to the completed spot file
                 comp_spot_output.write("{}\n".format(spot))
 
-        logger.info("Whole operation took: %s" % (datetime.now() - begin))
+
+def extract_id(page):
+    page_data = []
+    for doc in page['hits']['hits']:
+        doc_info = doc['_source']['info']
+        id =  doc['_id']
+        fpath = os.path.join(doc_info['directory'],doc_info['name'])
+        page_data.append('{},{}\n'.format(id,fpath))
+    return page_data
+
+
+def write_page_to_file(page,page_no, output_dir):
+
+    if not os.path.isdir(output_dir):
+        os.mkdir(output_dir)
+
+    with open(os.path.join(output_dir,"md5_update_page_{}.txt".format(page_no)),'w') as writer:
+        writer.writelines(extract_id(page))
+
+def download_files_missing_md5(index,host,port, output_dir):
+
+    scroll_window = 10000
+
+    query = json.dumps({
+        "query": {
+            "bool": {
+                "must": [
+                    {"term": {
+                        "info.md5": {
+                            "value": ""
+                        }
+                    }}
+                ]
+            }
+        }
+    })
+
+
+    esu = ElasticsearchUpdater(index, host, port)
+
+    page_no = 1
+    # Initialize the scroll
+    page = esu.es.search(
+        index=esu.index,
+        scroll='2m',
+        size=scroll_window,
+        body=query)
+
+    sid = page['_scroll_id']
+    scroll_size = page['hits']['total']
+    max_pages = math.ceil(scroll_size/float(scroll_window))
+    pb = ProgressBar(max_pages)
+    write_page_to_file(page, page_no, output_dir)
+
+    # Start scrolling
+    while (scroll_size > 0):
+        page = esu.es.scroll(scroll_id=sid, scroll='2m')
+        # Update the scroll ID
+        sid = page['_scroll_id']
+        # Update page_no
+        page_no += 1
+        pb.running(page_no)
+
+        # Get the number of results that we returned in the last scroll
+        scroll_size = len(page['hits']['hits'])
+        # Do something with the obtained page
+        write_page_to_file(page, page_no)
+    pb.complete()
+
+
+def calculate_md5s(arguments):
+    index = arguments["INDEX"]
+    log_dir = arguments["OUTPUT"]
+    host = arguments["HOSTNAME"]
+    port = arguments["PORT"]
+
+    document_output = 'page_files'
+
+    # Download ES id and filepath to local file.
+    download_files_missing_md5(index, host, port, document_output)
+
+    # Submit those files as jobs to lotus
+    for file in os.listdir(document_output):
+        command = 'md5.py -i {index} -o {log_dir} --pagefile {page}'.format(
+            index=index,
+            log_dir=log_dir,
+            page=file)
+
+        subprocess.call("bsub -q short-serial -W 24:00 {}".format(command),shell=True)
+
+
+
+def main():
+    arguments = docopt(__doc__, version=__version__)
+
+    # Extract commandline args.
+    index = arguments["INDEX"]
+    log_dir = arguments["OUTPUT"]
+
+    if arguments["HOSTNAME"] == None:
+        arguments["HOSTNAME"] = "jasmin-es1.ceda.ac.uk"
+
+
+    if arguments["PORT"] == None:
+        arguments["PORT"] = 9200
+
+    # setup logging
+    logger = logger_setup(log_dir)
+
+    logger.info("Updating {} index with md5 checksums.".format(index))
+
+    # Start time
+    begin = datetime.now()
+
+    if arguments["-c"] or arguments["--calculate"]:
+        calculate_md5s(arguments)
+    else:
+        update_from_logs(arguments, index, log_dir)
+
+
+
+    logger.info("Whole operation took: %s" % (datetime.now() - begin))
 
 
 if __name__ == "__main__":
