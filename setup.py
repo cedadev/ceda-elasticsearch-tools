01ead66ad497eb00871549b093c027be483021e3
Richard Smith <richard.d.smith@stfc.ac.uk>
2019-02-07 12:21:33 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.3.7',
+    version='0.4.0',
 
     description='Extension of the basic elasticsearch python wrapper to perform operations with a given ES index.',
     long_description=long_description,
