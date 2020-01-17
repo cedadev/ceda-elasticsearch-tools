d5921935652d75a2b73e364f885dc10fdb38f38f
rsmith013 <richard.d.smith@stfc.ac.uk>
2020-01-17 15:56:56 +0000

    added root certificate to package data

--- ceda_elasticsearch_tools/elasticsearch/__init__.py
+++ ceda_elasticsearch_tools/elasticsearch/__init__.py
@@ -8,4 +8,4 @@ __copyright__ = 'Copyright 2018 United Kingdom Research and Innovation'
 __license__ = 'BSD - see LICENSE file in top-level package directory'
 __contact__ = 'richard.d.smith@stfc.ac.uk'
 
-from ceda_elasticsearch_client import CEDAElasticsearchClient
+from .ceda_elasticsearch_client import CEDAElasticsearchClient
--- setup.py
+++ setup.py
@@ -66,6 +66,9 @@ setup(
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    package_data={
+        'ceda_elasticsearch_tools': ['root_certificate/root-ca.pem']
+    },
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
