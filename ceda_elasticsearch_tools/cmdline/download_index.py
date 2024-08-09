79eea4dde59d79545dbd7729aa53e2f3ff54d603
rhysrevans3 <rhys.r.evans3@gmail.com>
2024-08-09 15:16:39 +0100

    Adding mapping flag.

--- ceda_elasticsearch_tools/cmdline/download_index.py
+++ ceda_elasticsearch_tools/cmdline/download_index.py
@@ -7,6 +7,7 @@ Usage:
     download_index.py
                    (-i INDEX        | --index INDEX )
                    (-o OUTPUT       | --output OUTPUT )
+                   [-m              | --mapping ]
                    [-s SIZE         | --size SIZE ]
                    [-a API_KEY      | --api-key API_KEY ]
 
@@ -16,6 +17,7 @@ Options:
     --version           Show Version.
     -i  --index         Elasticsearch index to download.
     -o  --output        Script output directory.
+    -m  --mapping       Flag if mapping should be saved.
     -h  --hostname      Elasticsearch host to query [default: elasticsearch.ceda.ac.uk]
     -s  --size          Number of records per query [default: 10000]
     -a  --api-key       API key for Elasticsearch.
@@ -48,12 +50,26 @@ def search(es, config, search_after):
     )
 
 
-def main():
-    # Parse the command line arguments
-    config = docopt(__doc__, version=__version__)
+def dump(path, data):
+    with open(
+        file=path,
+        mode="w+",
+        encoding="utf-8",
+    ) as file:
+        json.dump(data, file)
 
-    es = CEDAElasticsearchClient(headers={"x-api-key": config["API_KEY"]})
 
+def get_mapping(es, config):
+    mapping = es.indices.get_mapping(index=config["INDEX"])
+
+    mapping_path = os.path.join(config["OUTPUT"], "mapping.json")
+
+    dump(mapping, mapping_path)
+
+    print("Mapping saved")
+
+
+def records(es, config):
     search_after = []
 
     record_count = 0
@@ -68,20 +84,26 @@ def main():
         for record in result["hits"]["hits"]:
             record_count += 1
             file_path = os.path.join(config["OUTPUT"], record["_id"])
-
-            with open(
-                file=file_path,
-                mode="w+",
-                encoding="utf-8",
-            ) as record_file:
-                json.dump(record, record_file)
-                file_count += 1
+            dump(record, file_path)
+            file_count += 1
 
         search_after = [result["hits"]["hits"][-1]["_id"]]
 
     print(
         f"Total Records: {record_count} Total Files created: {file_count} Total Missed: {record_count - file_count} Percent Missing: {((record_count - file_count)/record_count)*100}%"
     )
+
+
+def main():
+    # Parse the command line arguments
+    config = docopt(__doc__, version=__version__)
+
+    es = CEDAElasticsearchClient(headers={"x-api-key": config["API_KEY"]})
+
+    if config["MAPPING"]:
+        get_mapping(es, config)
+
+    records(es, config)
 
 
 if __name__ == "__main__":
